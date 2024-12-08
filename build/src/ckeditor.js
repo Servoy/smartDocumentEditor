fb213c38360a67187a941db246513cdd84dd6138
RvVeen <rvanveen@servoy.com>
2024-12-08 22:52:03 +0100

    Add support for Autolink Feature (TiNG version)

--- build/src/ckeditor.js
+++ build/src/ckeditor.js
@@ -37,6 +37,7 @@ import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
 import Link from '@ckeditor/ckeditor5-link/src/link';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
@@ -98,6 +99,7 @@ DecoupledEditor.builtinPlugins = [
 	Indent,
 	IndentBlock,
 	Link,
+    AutoLink,
 	List,
 	MediaEmbed,
 	Paragraph,
